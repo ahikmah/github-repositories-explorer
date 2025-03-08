a8ae77e60668f12ef92120c75cdba567b6d72018
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-08 17:32:04 +0700

    feat: enhance Footer component with source code link and style updates

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -2,10 +2,18 @@ const Footer = () => {
   return (
     <footer className="container mx-auto px-4 max-w-screen-lg text-center fixed bottom-0 left-0 right-0">
       <div className="flex justify-center">
-        <p className="text-sm text-gray-500">
+        <p className="text-xs text-gray-500">
           Build with 😃 by{" "}
           <a href="https://ahikmah.com" target="_blank" rel="noopener">
-            ahikmah
+            <span className="font-bold text-cyan-600">ahikmah</span>
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/ahikmah/github-repositories-explorer.git"
+            target="_blank"
+            rel="noopener"
+          >
+            <span className="font-bold text-cyan-600">View Source Code</span>
           </a>
         </p>
       </div>

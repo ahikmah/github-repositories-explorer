1833ba38e9365bd15b04d7f7ceba9ace8e317629
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-08 16:51:57 +0700

    feat: refactor ThemeToggler to use updated context and component imports

--- src/components/theme-toggler/theme-toggler.tsx
+++ src/components/theme-toggler/theme-toggler.tsx
@@ -1,23 +1,17 @@
-import { useContext } from "react";
-import { ThemeContext } from "src/theme";
-import { Icon } from "@iconify/react";
+import { useThemeContext } from "src/theme";
+import { Iconify } from "src/components/iconify";
 
 export const ThemeToggler: React.FC = () => {
-  const themeContext = useContext(ThemeContext);
-  if (!themeContext) return null;
+  const { theme, toggleTheme } = useThemeContext();
 
   return (
     <button
       type="button"
-      title={
-        themeContext.theme === "light"
-          ? "Switch to dark mode"
-          : "Switch to light mode"
-      }
-      className="px-4 py-2 rounded-lg bg-gray-300 dark:bg-gray-950 text-gray-800 dark:text-white"
-      onClick={themeContext.toggleTheme}
+      title={theme === "light" ? "Switch to dark mode" : "Switch to light mode"}
+      className="p-3 rounded-lg bg-slate-300 dark:bg-slate-900 text--800 dark:text-slate-300"
+      onClick={toggleTheme}
     >
-      <Icon icon={themeContext.theme === "light" ? "bi:moon" : "bi:sun"} />
+      <Iconify icon={theme === "light" ? "bi:moon" : "bi:sun"} size={16} />
     </button>
   );
 };

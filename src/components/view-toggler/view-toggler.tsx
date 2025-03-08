8be4af6ece073fa11017b5416272290d6cece338
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-08 16:52:37 +0700

    feat: add Toggle and ViewToggler components with context support

---
+++ src/components/view-toggler/view-toggler.tsx
@@ -0,0 +1,35 @@
+import { Iconify } from "src/components/iconify";
+import { useThemeContext } from "src/theme";
+import { ToggleGroup, ToggleGroupItem } from "src/components/ui/toggle-group";
+
+export const ViewToggler = () => {
+  const { viewMode, toggleViewMode } = useThemeContext();
+
+  return (
+    <ToggleGroup variant="outline" type="single" value={viewMode} size="lg">
+      <ToggleGroupItem
+        value="mobile"
+        aria-label="Toggle mobile view"
+        onClick={() => toggleViewMode("mobile")}
+      >
+        <div
+          className={viewMode === "mobile" ? "text-cyan-600" : "text-gray-400"}
+        >
+          <Iconify icon="tabler:device-mobile" size={24} />
+        </div>
+      </ToggleGroupItem>
+      <ToggleGroupItem
+        value="system"
+        aria-label="Toggle system view"
+        onClick={() => toggleViewMode("system")}
+      >
+        <div
+          className={viewMode === "system" ? "text-cyan-600" : "text-gray-400"}
+        >
+          <Iconify icon="tabler:device-desktop" size={24} />
+        </div>
+      </ToggleGroupItem>
+    </ToggleGroup>
+  );
+};
+export default ViewToggler;

b2fd74e6bf713eb3d213f672792fff133c36751f
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-08 19:27:00 +0700

    fix: update BrowserRouter basename for GitHub repositories explorer

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/github-repositories-explorer">
       <App />
     </BrowserRouter>
   </StrictMode>

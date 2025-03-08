9c24e4fa67abf4e1a21c1f350913a946ddd63aa4
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-08 16:49:07 +0700

    feat: add ViewToggler component to Header and adjust Logo size

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Logo } from "src/components/logo/logo";
 import { ThemeToggler } from "src/components/theme-toggler";
+import { ViewToggler } from "src/components/view-toggler";
 
 export const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 z-10 shadow-sm dark:bg-gray-800 bg-gray-100 text-white">
+    <header className="sticky top-0 z-10 shadow-sm dark:bg-gray-800 bg-gray-100 text-gray-600 dark:text-gray-100">
       <div className="container mx-auto px-4 flex items-center justify-between py-2 max-w-screen-lg">
-        <Logo width="70px" height="70px" />
+        <Logo width="60px" height="60px" />
+
+        <div className="hidden md:flex items-center">
+          <ViewToggler />
+        </div>
+
         <ThemeToggler />
       </div>
     </header>

fd775c4578728a7f65c353fbafb86f9a41ed90c5
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-09 14:17:43 +0700

    feat: add TypeScript interfaces for Repository and User types

---
+++ src/types/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  login: string;
+  id: number;
+  avatar_url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  repos_url: string;
+  organizations_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
+}

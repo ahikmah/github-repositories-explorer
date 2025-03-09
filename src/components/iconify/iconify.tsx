9505b54085a36079e6b8385267197d23bb680aad
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-09 14:13:55 +0700

    feat: add className prop to Iconify component for custom styling

--- src/components/iconify/iconify.tsx
+++ src/components/iconify/iconify.tsx
@@ -3,12 +3,14 @@ import { Icon } from "@iconify/react";
 interface IconifyProps {
   icon: string;
   size?: string | number;
+  className?: string;
 }
 
-export const Iconify = ({ icon, size = 14 }: IconifyProps) => {
+export const Iconify = ({ icon, size = 14, className }: IconifyProps) => {
   return (
     <Icon
       icon={icon}
+      className={className}
       style={{
         width: size,
         height: size,

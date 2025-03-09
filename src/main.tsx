8873480264e8e24b367bd77c59210d79e11a2209
Awaliyatul Hikmah <imadlz15@gmail.com>
2025-03-09 14:19:01 +0700

    feat: integrate React Query by adding QueryClientProvider to main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,10 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./app";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename={import.meta.env.VITE_BASE_PATH || "/"}>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
